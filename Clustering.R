87392100bc61a9b1dd7bbef162762a427a38e2c0
Afaf saaidi <afaf.saaidi@gmail.com>
2021-05-26 19:30:19 +0100

    adding new plots

--- Clustering.R
+++ Clustering.R
@@ -75,7 +75,7 @@ k3 <-kmeans(Rankedhelices$averageRLtdiff, centers=3) #Create3 clusters
 k3$cluster
 Numberofhelicesinoptimalcluster<-table( k3$cluster)[names(table (k3$cluster)) == k3$cluster[1]]
 Numberofhelicesinoptimalcluster
-Newdata<-data.frame(target,k$cluster,Rankedhelices$Statt)
+Newdata<-data.frame(target,Rankedhelices$averageRLtdiff,k$cluster,Rankedhelices$Statt)
 write.csv(Newdata,'RedMaxHoutput/Kmeans_elbow_mean.csv')
 Newdata3<-data.frame(target,k3$cluster,Rankedhelices$Statt)
 
@@ -129,4 +129,26 @@ multi.page <- ggarrange(p1, p2, g,
 ggexport(multi.page, filename = "RedMaxPlots/Clusters_Helices.pdf", width =30.0, height = 15)
 
 
+
+ggexport(p1, filename = "RedMaxPlots/Elbow_Clusters_Helices.pdf", width =30.0, height = 15)
+
+
+
+
+myPalette <- colorRampPalette(brewer.pal(11, "Spectral"))
+sc <- scale_colour_gradientn(name ="Average R",colours = myPalette(100), limits=c(-1, 1))
+p1<-ggplot(contributingpairsX,aes(x=1,y=Rforpairs,colour=Rankedhelices.averageRLtdiff))+#as.factor(Helix))) +
+  sc+
+  ggtitle("Kmeans on Mean, nbr clusters (elbow)")+
+  geom_violin(colour = "grey50")+
+  geom_boxplot(width=0.1,colour = "grey50")+
+  stat_summary(fun.y=mean, geom="point", size=2, color="black")+
+  geom_jitter( size = 3)+ylim(-1,3)+
+  
+  facet_wrap(~as.factor(contributingpairsX$Helix),nrow = 1)
+
+p1<-p1+ theme(legend.position = "bottom",panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+              panel.background = element_blank(), axis.line = element_line(colour = "black"),axis.title.x = element_blank(),axis.title.y = element_blank(), axis.text.x=element_blank())
+
+ggexport(p1, filename = "RedMaxPlots/Color_by_average_Helices.pdf", width =30.0, height = 15)
 

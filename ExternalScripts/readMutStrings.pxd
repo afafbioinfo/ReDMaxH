7d55fb210fa921e74969a937db8d575a82e5608e
Afaf saaidi <afaf.saaidi@gmail.com>
2021-10-07 20:30:06 +0100

    back to the source files

---
+++ ExternalScripts/readMutStrings.pxd
@@ -0,0 +1,14 @@
+cdef struct READ:
+    int start
+    int stop
+    int subcode
+    char* read
+    char* muts
+    char* qual
+
+
+cdef READ parseLine(char*, int)
+
+cdef void fillReadMut(int[:], int[:], READ, int, int)
+
+cdef void incrementArrays(int[:,::1], int[:,::1], int[:,::1], int[:], int[:])
---
+++ ExternalScripts/setup.py
@@ -0,0 +1,20 @@
+#########################################################
+# Code to compile readMutStrings.pyx
+# Anthony Mustoe
+# 2018
+#
+# This file is licensed under the terms of the MIT license  
+#
+#########################################################
+
+
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Build import cythonize
+import numpy
+
+ext = Extension('readMutStrings', 
+                sources=['readMutStrings.pyx'], 
+                include_dirs = [numpy.get_include()])
+
+setup(name = 'readMutStrings', ext_modules = cythonize(ext))
